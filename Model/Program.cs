21d203237632052b6a898e0ede916a3241e066b7
Breno Gonçalves <42100181+DeveloperBreno@users.noreply.github.com>
2024-03-11 09:24:20 -0300

    Removendo pastas não utilizadas

--- Model/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Model;
-
-using (var db = new EluminiDbContext())
-{
-    db.Database.Migrate();
-    Console.WriteLine("Migrations applied successfully!");
-}
